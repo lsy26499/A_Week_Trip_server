6827558a419236a9be4b36a9288ba6fe1bee4eb0
Heo-JiSeong <heojiseong0811@gmail.com>
2020-07-22 11:19:18 +0900

    :card_file_box: Add userId in plan Schema
    - userId를 추가하였습니다.

--- model/plan.js
+++ model/plan.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const planSchema = new Schema({
-  info: Array,
-  created_at: { type: Date, required: true, default: Date.now },
-  updated_at: { type: Date, required: true, default: Date.now },
+    userId: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    info: Array,
+    created_at: { type: Date, required: true, default: Date.now },
+    updated_at: { type: Date, required: true, default: Date.now },
 });
 
 const Plan = mongoose.model('Plan', planSchema);
