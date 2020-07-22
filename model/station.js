2ae81804646f4e585a2db8ea524b4d4aea0869cb
idoasisay <59815596+idoasisay@users.noreply.github.com>
2020-07-22 21:27:21 +0900

    :bug: Fix station Schema
    - 누락된 stationNumber 필드를 생성했습니다.

--- model/station.js
+++ model/station.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
+
 const Schema = mongoose.Schema;
 
 const stationSchema = new Schema({
     station: { type: String, required: true },
+    stationNumber: { type: Number, required: true },
     info: { type: String, required: true },
     lodging: { type: Array, required: true },
-    tourism: Array,
-    food: Array,
+    tourism: { type: Array, required: true },
+    food: { type: Array, required: true },
 });
 
 const Station = mongoose.model('Station', stationSchema);
