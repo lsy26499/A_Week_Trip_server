a08cd2578046c857ce9c3553501ed2a5824fe15f
Heo-JiSeong <heojiseong0811@gmail.com>
2020-07-22 11:20:51 +0900

    :card_file_box: Fix model files
    - Preetier로 인해 들여쓰기가 적용되었습니다.

--- model/comment.js
+++ model/comment.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
-  userId: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-  communityId: [{ type: Schema.Types.ObjectId, ref: 'Community' }],
-  comment: String,
-  created_at: { type: Date, required: true, default: Date.now },
-  updated_at: { type: Date, required: true, default: Date.now },
+    userId: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    communityId: [{ type: Schema.Types.ObjectId, ref: 'Community' }],
+    name: String,
+    comment: String,
+    created_at: { type: Date, required: true, default: Date.now },
+    updated_at: { type: Date, required: true, default: Date.now },
 });
 
 const Comment = mongoose.model('Comment', commentSchema);
--- model/user.js
+++ model/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  // User Login API 구현 시 채울 것
+    // User Login API 구현 시 채울 것
 });
 
 const User = mongoose.model('User', userSchema);
