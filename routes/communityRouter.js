e0ffef353565b8abcfc0e664a20350f2714a4fae
idoasisay <59815596+idoasisay@users.noreply.github.com>
2020-07-23 16:37:39 +0900

    :construction: Work commu router
    - 커뮤니티 라우터를 작업 중입니다.

--- routes/communityRouter.js
+++ routes/communityRouter.js
@@ -6,10 +6,12 @@ import postEdit from '../controller/communityController/postEdit';
 import postList from '../controller/communityController/postList';
 import postView from '../controller/communityController/postView';
 
-export const communityRouter = express.Router();
+const communityRouter = express.Router();
 
 // communityRouter.post('/', postCreate); // 생성
 // communityRouter.delete('/:id', postDelete); // 지움
-// communityRouter.put('/:id', postEdit); // 수정
+communityRouter.put('/:id', postEdit); // 수정
 // communityRouter.get('/', postList); // 전체 보여 주기
 // communityRouter.get('/:id', postView); // 하나만 보여 주기
+
+export default communityRouter;
