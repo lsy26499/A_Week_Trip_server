eabd56bcabe4af48ab0fa5028857d9e99fd73abd
Heo-JiSeong <heojiseong0811@gmail.com>
2020-07-23 23:50:26 +0900

    :sparkles: Add postView Get API
    - postView Get API 생성

---
+++ controller/communityController/postView.js
@@ -0,0 +1,23 @@
+import Community from '../../model/community';
+
+const postView = async (req, res) => {
+    const { _id } = req.params;
+    try {
+        const postView = await Community.find(
+            { _id: _id },
+            {
+                _id: true,
+                userId: true,
+                name: true,
+                title: true,
+                article: true,
+                created_at: true,
+            }
+        );
+        res.status(200).send(postView);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+};
+
+export default postView;
