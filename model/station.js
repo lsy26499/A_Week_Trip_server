13c55226bf3224defb2ed96487d1090cd1222c6d
idoasisay <59815596+idoasisay@users.noreply.github.com>
2020-07-23 14:52:59 +0900

    :sparkles: Add infoDetail Schema for lodging, tourlism, food filed
    - 중복 스키마를 삭제하고 하나로 선언한 뒤 값을 넣어 주었습니다.

--- model/station.js
+++ model/station.js
@@ -2,15 +2,27 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const infoDetailSchema = new Schema({
+    _id: false,
+    name: String,
+    info: String,
+    phone: Number,
+    address: String,
+});
 const stationSchema = new Schema({
     station: { type: String, required: true },
     stationNumber: { type: Number, required: true },
-    info: { type: String, required: true },
-    lodging: { type: Array, required: true },
-    tourism: { type: Array, required: true },
-    food: { type: Array, required: true },
+    stationInfo: { type: String, required: true },
+    lodging: [infoDetailSchema],
+    tourism: [infoDetailSchema],
+    food: [infoDetailSchema],
 });
 
 const Station = mongoose.model('Station', stationSchema);
 
 export default Station;
+
+/*
+    created_at: { type: Date, required: true, default: new Date().getTime() },
+    updated_at: { type: Date, required: true, default: new Date().getTime() },
+*/
