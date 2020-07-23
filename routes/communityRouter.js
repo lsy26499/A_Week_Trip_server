0fee47111fb75f798fab6e8aac9db5a2cf9c3800
idoasisay <59815596+idoasisay@users.noreply.github.com>
2020-07-23 21:50:13 +0900

    :wrench: Add objectid Vaildation middleware
    - object id 유효성을 검사하는 미들웨어를 추가하였습니다.

--- routes/communityRouter.js
+++ routes/communityRouter.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { checkObjectId } from '../middlewares';
 
 import postCreate from '../controller/communityController/postCreate';
 import postDelete from '../controller/communityController/postDelete';
@@ -10,7 +11,7 @@ const communityRouter = express.Router();
 
 // communityRouter.post('/', postCreate); // 생성
 // communityRouter.delete('/:id', postDelete); // 지움
-communityRouter.put('/:id', postEdit); // 수정
+communityRouter.put('/:id', checkObjectId, postEdit); // 수정
 // communityRouter.get('/', postList); // 전체 보여 주기
 // communityRouter.get('/:id', postView); // 하나만 보여 주기
 
