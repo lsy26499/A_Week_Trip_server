a426a8279e5e5230c9861666071c329926f5b98c
idoasisay <59815596+idoasisay@users.noreply.github.com>
2020-07-25 21:00:43 +0900

    :wrench: Modify comment & community Schema
    - 코멘트와 커뮤니티 스키마를 변경한 설정에 맞게 수정했습니다.

--- model/comment.js
+++ model/comment.js
@@ -1,13 +1,22 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const commentSchema = new Schema({
-    userId: { type: Number, require: true },
-    //userId: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-    name: String,
-    comment: String,
-    created_at: { type: Date, required: true, default: Date.now },
-});
+const commentSchema = new Schema(
+    {
+        userId: { type: Number, require: true },
+        //userId: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+        name: String,
+        comment: String,
+        communityID: { type: Schema.Types.ObjectId, ref: 'Community' },
+        createdAt: {
+            type: Date,
+            required: true,
+            default: Date.now,
+        },
+        //created_at: { type: Date, required: true, default: date },
+    },
+    { timestamps: { createdAt: 'createdAt' } }
+);
 
 const Comment = mongoose.model('Comment', commentSchema);
 
