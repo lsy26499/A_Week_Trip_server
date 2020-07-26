6d493be9ef04f6dc957ddad210280f8c3b50f505
idoasisay <59815596+idoasisay@users.noreply.github.com>
2020-07-26 16:36:09 +0900

    :wrench: Add weather API
    - 해당 지역의 날씨를 표시해 주는 API를 생성했습니다.

--- controller/stationController/stationDetail.js
+++ controller/stationController/stationDetail.js
@@ -1,4 +1,22 @@
 import Station from '../../model/station';
+import dotenv from 'dotenv';
+import request from 'request';
+
+dotenv.config();
+
+const weatherIcon = (lat, lon, callback) => {
+    request(
+        `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=` +
+            process.env.WEATHER_API_KEY,
+        function (err, res, body) {
+            if (err) console.log(err);
+            else {
+                let obj = JSON.parse(body);
+                callback(obj);
+            }
+        }
+    );
+};
 
 const stationDetail = async (req, res) => {
     const { id } = req.params;
@@ -8,7 +26,16 @@ const stationDetail = async (req, res) => {
             res.status(404).send('404 Not found');
             return;
         }
-        res.status(200).send(stationDetail);
+        const { lon, lat } = await stationDetail[0].coord;
+
+        weatherIcon(lon, lat, (data) => {
+            const icon = data.weather[0].icon;
+
+            res.status(200).send({
+                stationDeatil: stationDetail,
+                weather: `http://openweathermap.org/img/wn/${icon}@2x.png`,
+            });
+        });
     } catch (err) {
         console.log(err);
         res.send(err);
