1edb61297143e298182d18dd4ddbd69f4cf94b97
idoasisay <59815596+idoasisay@users.noreply.github.com>
2020-07-27 13:07:52 +0900

    :wastebasket: CleanUp for Community post
    - community post 클린업 작업

--- controller/communityController/postSearch.js
+++ controller/communityController/postSearch.js
@@ -1,20 +1,25 @@
 import Community from '../../model/community';
 
+//POST
 const postSearch = async (req, res) => {
     const { content } = req.query;
+
     let options = [
         { title: new RegExp(content) },
         { article: new RegExp(content) },
     ];
+
     try {
         const community = await Community.find({ $or: options });
         if (!community) {
             console.log('검색 옵션이 없습니다.');
             res.status(400).send('검색 옵션이 없습니다.');
         }
-        res.status(200).send(community);
+        res.status(201).send(community);
     } catch (err) {
         res.status(500).send(err);
+    } finally {
+        res.end();
     }
 };
 
