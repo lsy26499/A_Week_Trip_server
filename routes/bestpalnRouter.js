15848d116971c11d5735abe091e9c4cc61952fdb
idoasisay <59815596+idoasisay@users.noreply.github.com>
2020-07-28 19:15:58 +0900

    :pencil2: Fix typos in bestplan Router
    - 띄어쓰기 오타를 수정했습니다.

--- routes/bestpalnRouter.js
+++ routes/bestpalnRouter.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import courseList from '../controller/bestplanController/ courseList';
+import courseList from '../controller/bestplanController/courseList';
 
 const bestplanRouter = express.Router();
 
