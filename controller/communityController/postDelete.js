a2341e30b7891840cba7601b7f45ffaa88d4f932
idoasisay <59815596+idoasisay@users.noreply.github.com>
2020-07-29 13:40:26 +0900

    :wrench: Update posts delete for scrap post
    - 게시글을 삭제하면 게시글 스크랩 배열에 있는 게시글도 삭제가 될 수 있게 처리했습니다.

--- controller/communityController/postDelete.js
+++ controller/communityController/postDelete.js
@@ -15,17 +15,16 @@ const postDelete = async (req, res) => {
     try {
         await Post.findByIdAndRemove(id);
 
-        /**
-         * 전체 유저 중에서 이 게시글을 scrapPosts한 유저를 찾는다.
-         * user.scrapPosts를 pop해 줘야 되는데 map으로 돌려서 삭제를 해야 되나?
-         *
-         * FIXME: scrapPosts 배열 안의 해당 게시글을 전부 삭제할 수 있게 해야 합니다.
-         */
+        const user = await User.find({ scrapPosts: ObjectID(id) });
 
-        const user = await User.find({ scrapPosts: id });
         if (user) {
-            // user.scrapPosts.pop({ _id: ObjectID(id) });
-            // user.save();
+            await User.updateMany(
+                { scrapPosts: ObjectID(id) },
+                {
+                    $pull: { scrapPosts: ObjectID(id) },
+                },
+                { new: true }
+            );
         }
         res.status(200).send('성공적으로 게시글이 삭제되었습니다.');
     } catch (err) {
