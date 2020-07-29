8fa8237a6e172e726c0b4154cc4cfeb0b0d72a1f
idoasisay <59815596+idoasisay@users.noreply.github.com>
2020-07-29 16:29:07 +0900

    :wrench: Update Schema filed for favStation
    - 스키마 필드를 post에서 favStation으로 업데이트하였습니다.

--- model/user.js
+++ model/user.js
@@ -8,9 +8,10 @@ const userSchema = new Schema({
     // User Login API 구현 시 채울 것
     // 내가 작성한 게시글 삽입을 위한 Fake userId
     userId: { type: Number, require: true },
-    posts: [{ type: Schema.Types.ObjectId, ref: 'Community' }],
+    favStation: [{ type: Schema.Types.ObjectId, ref: 'Station' }],
     scrapPosts: [{ type: Schema.Types.ObjectId, ref: 'communitys' }],
     //? FIXME: populate는 "mongoDB 기준 필드명"으로 적용됩니다. populate를 쓰실 거면 ref를 바꾸어야 합니다.
+    //? ????????????? community만 그렇습니다. 이상...... 합니다.
 });
 
 const User = mongoose.model('User', userSchema);
