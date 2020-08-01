eea4564eb8eb9b6d4e639658e0a60fbb53c73729
idoasisay <59815596+idoasisay@users.noreply.github.com>
2020-08-01 22:12:05 +0900

    :bulb: Add JS DOCS comments for station, comment API
    - 스테이션과 코멘트 API에 API DOCS 주석을 추가했습니다.

--- controller/commentController/commentDelete.js
+++ controller/commentController/commentDelete.js
@@ -2,6 +2,19 @@ import Comment from '../../model/comment';
 import mongoose from 'mongoose';
 const { ObjectId } = mongoose.Types;
 
+/**
+ * @api {delete} /comment/:commentId 코멘트 삭제
+ * @apiDescription 기존의 코멘트를 삭제합니다.
+ * @apiName 코멘트 삭제
+ * @apiGroup comment
+ *
+ * @param {ObjectID} commentId 해당 댓글 오브젝트 아이디
+ *
+ * @apiSuccess {Number} 201 댓글 삭제 성공
+ * @apiError {Number} 400 댓글 아이디가 없음
+ * @apiError {Number} 500 댓글 삭제 실패
+ */
+
 const commentDelete = async (req, res) => {
     const { commentId } = req.params;
 
