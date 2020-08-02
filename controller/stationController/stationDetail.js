606b14fc1b64931c4ce9ce0c522c436a173889c1
idoasisay <59815596+idoasisay@users.noreply.github.com>
2020-08-03 02:41:29 +0900

    :wrench: Add WebCrawling for station infomation
    - 실시간 웹크롤링 함수를 만들고, 데이터를 전송할 때 함께 보낼 수 있게 API를 수정하였습니다.

--- controller/stationController/stationDetail.js
+++ controller/stationController/stationDetail.js
@@ -1,23 +1,71 @@
 import Station from '../../model/station';
 import dotenv from 'dotenv';
 import request from 'request';
+import cheerio from 'cheerio';
+
 import { ObjectID } from 'mongodb';
 
 dotenv.config();
 
-// 외부 API 받는 함수
-const weatherIcon = (lat, lon, callback) => {
-    request(
-        `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=` +
-            process.env.WEATHER_API_KEY,
-        async function (err, response, body) {
-            if (err) console.log(err);
-            else {
-                let obj = await JSON.parse(body);
-                callback(obj);
+// 날씨 API
+const weatherIcon = (lat, lon) => {
+    return new Promise((resolve) => {
+        request(
+            `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=` +
+                process.env.WEATHER_API_KEY,
+            async function (err, response, body) {
+                if (err) console.log(err);
+                else {
+                    let obj = await JSON.parse(body);
+                    const icon = await obj.weather[0].icon;
+                    resolve(icon);
+                }
             }
-        }
-    );
+        );
+    });
+};
+
+// 웹 크롤링
+const getInfo = async (region, subject) => {
+    try {
+        let ulList = [];
+        return new Promise((resolve) => {
+            request(
+                `https://search.naver.com/search.naver?where=post&sm=tab_jum&query=${encodeURIComponent(
+                    region
+                )}+${encodeURIComponent(subject)}`,
+                (err, res, body) => {
+                    if (err) console.log(err);
+                    else {
+                        const $ = cheerio.load(body);
+                        const $bodyList = $('ul.type01')
+                            .children('li.sh_blog_top')
+                            .slice(0, 5);
+
+                        $bodyList.each(function (i, elem) {
+                            ulList[i] = {
+                                title: $(this).find('dl dt a').text(),
+                                blogName: $(this)
+                                    .find('span.inline a.txt84')
+                                    .slice(0, 1)
+                                    .text(),
+                                description: $(this)
+                                    .find('dd.sh_blog_passage')
+                                    .text(),
+                                date: $(this).find('dd.txt_inline').text(),
+                                href: $(this).find('dl dt a').attr('href'),
+                            };
+                        });
+
+                        const data = ulList.filter((n) => n.title);
+                        resolve(data);
+                    }
+                }
+            );
+        });
+    } catch (err) {
+        console.log(err);
+    }
 };
 
 //GET
@@ -35,35 +83,34 @@ const stationDetail = async (req, res) => {
                     station: 1,
                     stationNumber: 1,
                     info: 1,
-                    lodging: 1,
-                    tourism: 1,
-                    food: 1,
                     lon: { $arrayElemAt: ['$coord', 0] },
                     lat: { $arrayElemAt: ['$coord', 1] },
                 },
             },
         ]);
-        const { lon, lat } = stationDetail[0];
+        const { lon, lat, station } = stationDetail[0];
+
+        const icon = await weatherIcon(lon, lat);
+        const tourism = await getInfo(station, '볼거리');
+        const food = await getInfo(station, '맛집');
+        const lodging = await getInfo(station, '숙소+추천');
 
         if (!stationDetail || !id) {
             res.status(404).send('404 Not found');
             return;
         }
-        // 외부 API
-        weatherIcon(lon, lat, async (data) => {
-            const icon = await data.weather[0].icon;
-            console.log(icon);
-            await res.status(200).send({
-                stationDeatil: stationDetail,
-                weather: `http://openweathermap.org/img/wn/${icon}@2x.png`,
-            });
+        res.status(200).send({
+            stationDeatil: stationDetail,
+            weather: `http://openweathermap.org/img/wn/${icon}@2x.png`,
+            lodging: lodging,
+            tourism: tourism,
+            food: food,
         });
     } catch (err) {
         console.log(err);
         res.send(err);
     } finally {
-        //외부 API 호출을 할 땐... res.end()를 쓰지 말아주세요...
-        //res.end();
+        res.end();
     }
 };
 
