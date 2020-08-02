c17d39b28379eb68921518b8c2880b123be8f5d5
idoasisay <59815596+idoasisay@users.noreply.github.com>
2020-08-03 02:40:05 +0900

    :wrench: Update station Schema
    - 웹 크롤링 구현으로 인해 바뀐 스키마를 수정하였습니다.

--- model/station.js
+++ model/station.js
@@ -2,21 +2,10 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const infoDetailSchema = new Schema({
-    _id: false,
-    name: String,
-    info: String,
-    phone: Number,
-    address: String,
-});
-
 const stationSchema = new Schema({
     station: { type: String, required: true },
     stationNumber: { type: Number, required: true },
     stationInfo: { type: String, required: true },
-    lodging: [infoDetailSchema],
-    tourism: [infoDetailSchema],
-    food: [infoDetailSchema],
     coord: { any: Object },
 });
 
