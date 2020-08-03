b2a3471da7c22b05d6f881e4b9f20ac9f4d1366f
idoasisay <59815596+idoasisay@users.noreply.github.com>
2020-08-04 00:37:21 +0900

    :wrench: Update user Schema
    - SNS 연동 로그인 때문에 유저 스키마를 부분 수정하였습니다.

--- model/user.js
+++ model/user.js
@@ -3,15 +3,14 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
     //TODO: User Login API 구현 시 채울 것
-    userId: { type: Number, require: true },
-    email: String,
+    userId: { type: String, require: true },
+    name: { type: String, required: true },
     fbToken: String,
+    googleToken: String,
+    googleReToken: String,
     jsonWebToken: String,
     favStation: [{ type: Schema.Types.ObjectId, ref: 'Station' }],
     scrapPosts: [{ type: Schema.Types.ObjectId, ref: 'communitys' }],
-
-    //? FIXME: populate는 "mongoDB 기준 필드명"으로 적용됩니다. populate를 쓰실 거면 ref를 바꾸어야 합니다.
-    //? ????????????? community만 그렇습니다. 이상...... 합니다.
 });
 
 const User = mongoose.model('User', userSchema);
