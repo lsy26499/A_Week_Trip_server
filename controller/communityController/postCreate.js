318e62550e6f2fc22f6c9757b563015a694c8b5a
idoasisay <59815596+idoasisay@users.noreply.github.com>
2020-08-04 19:08:49 +0900

    :bulb: Update comments for multer
    - 원활한 작업을 위해 뮬터를 주석 처리했습니다.

--- controller/communityController/postCreate.js
+++ controller/communityController/postCreate.js
@@ -13,14 +13,14 @@ import Community from '../../model/community';
 const postCreate = async (req, res) => {
     const {
         body: { userId, name, title, article },
-        file: { location }, //!현재 S3 기준으로 코딩 중입니다.
+        //file: { location }, //!현재 S3 기준으로 코딩 중입니다.
     } = req;
     const postCreate = new Community({
         userId,
         name,
         title,
         article,
-        imageURL: location,
+        //imageURL: location,
     });
     try {
         if (userId) {
