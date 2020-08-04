20cfbe2e0002b484e4a794d3fe90e9c39c509891
idoasisay <59815596+idoasisay@users.noreply.github.com>
2020-08-04 20:45:10 +0900

    :sparkles: Add logout API
    - 로그아웃 API를 생성했습니다.

---
+++ controller/userController/logout.js
@@ -0,0 +1,20 @@
+import User from '../../model/user';
+
+const logout = async (req, res) => {
+    const { _id } = req.body.user;
+
+    const deleteToken = { jsonWebToken: '' };
+    try {
+        const user = await User.findByIdAndUpdate(_id, deleteToken, {
+            new: true,
+        });
+        console.log(user);
+        res.status(201).send('로그아웃이 정상적으로 처리되었습니다.');
+    } catch (err) {
+        console.log(err);
+    } finally {
+        res.end();
+    }
+};
+
+export default logout;
