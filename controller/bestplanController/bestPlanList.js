da48aba4d0aa07eab4f953330cb8ccf8f6869e84
idoasisay <59815596+idoasisay@users.noreply.github.com>
2020-08-09 15:29:05 +0900

    :bulb: ADD Comment JS DOCS
    - bestplan router에 js docs를 추가했습니다.

--- controller/bestplanController/bestPlanList.js
+++ controller/bestplanController/bestPlanList.js
@@ -1,5 +1,29 @@
 import BestPlan from '../../model/bestplan';
 
+/**
+ * @api {get} /bestplan
+ * @apiDescription 추천 코스 리스트를 요청합니다.
+ * @apiName 추천 코스 리스트 요청
+ * @apiGroup bestplan
+ *
+ * @apiSuccess {Number} 200 추천 코스 리스트 요청 성공
+ * @apiSuccessExample {json} Success-Response:
+ *       HTTP/1.1 200
+ * [
+ *   {
+ *     plan: [ [Object], [Object], [Object], [Object], [Object] ],
+ *     _id: 5f25373095677c0f07a867bb,
+ *     num: 1
+ *   },
+ *   {
+ *     plan: [ [Object], [Object], [Object], [Object], [Object] ],
+ *     _id: 5f253730934337c0f07a867bb,
+ *     num: 2
+ *   }
+ * ]
+ * @apiError {Number} 500 계획 요청 실패
+ */
+
 const bestPlanList = async (req, res) => {
     try {
         const bestPlanList = await BestPlan.aggregate([
